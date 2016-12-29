203e6ee1ae3cdee24d2cfbcc080068b93a015942
wx-6034 <wx-wangk@dtdream.com>
2016-12-29 14:30:49 +0800

    Define scanner

--- simple-rpc-common/src/main/java/com/rpc/common/scanner/ClassScanner.java
+++ simple-rpc-common/src/main/java/com/rpc/common/scanner/ClassScanner.java
@@ -1,8 +1,13 @@
 package com.rpc.common.scanner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by kaiwang on 2016/12/29.
  */
 public class ClassScanner {
-
+    public List<Class> doScan(String basePackage) {
+        return new ArrayList<>();
+    }
 }
